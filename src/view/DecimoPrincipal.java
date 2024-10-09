8237f987b1f52aef6bbdfd0078f0244b2caae069
014200096 <014200096@UNI014-NTI-003.sereduc.intra>
2024-10-09 16:40:08 -0300

    exercicio 9 e 10 lista avancada

--- src/view/DecimoPrincipal.java
+++ src/view/DecimoPrincipal.java
@@ -1,8 +1,40 @@
 package view;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.Scanner;
+
 public class DecimoPrincipal {
 /*10) Faça um algoritmo que preencha uma lista de 30 posições com números entre 1 e 15 sorteados pelo computador. 
  * 	  Depois disso, peça para o usuário digitar um número (chave) e seu programa deve mostrar em que posições essa chave foi encontrada.
  * 	  Mostre também quantas vezes a chave foi sorteada./
  */
+	
+	public static void main(String[] args) {
+		List<Integer> sorteadosUmQuize = new ArrayList<Integer>(30);
+		Random r = new Random();
+		Scanner s = new Scanner(System.in);
+
+		for (int i = 0; i < 30; i++) {
+			int n = r.nextInt(15) + 1;
+			sorteadosUmQuize.add(n);
+		}
+		
+		System.out.println("Digite um número: ");
+		int dig = s.nextInt();
+		
+		
+		/* não mostra qtd e vezes */
+		for (Integer chave : sorteadosUmQuize) {
+			if(chave == dig) {
+				if (chave != dig) {
+					System.out.println("Chave digitada não esta nessa lista.");
+				}
+				System.out.println("Posições: " +sorteadosUmQuize.indexOf(dig) + ", numero: " + sorteadosUmQuize.get(dig));
+			}
+		}
+		
+		System.out.println(sorteadosUmQuize);
+	}
 }
--- src/view/DecimoSegundoPrincipal.java
+++ src/view/DecimoSegundoPrincipal.java
@@ -1,5 +1,23 @@
 package view;
 
-public class DecimoSegundoPrincipal {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.Scanner;
 
+public class DecimoSegundoPrincipal {
+/*10) 	Faça um algoritmo que preencha uma lista de 30 posições com números entre 1 e 15 sorteados pelo computador. 
+ * 		Depois disso, peça para o usuário digitar um número (chave) e seu programa deve mostrar em que posições essa chave foi encontrada. 
+ * 		Mostre também quantas vezes a chave foi sorteada.
+ * */
+	
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		Random r = new Random();
+		List<Integer> Numeros = new ArrayList<Integer>();
+		
+		for (int i = 0; i < 30; i++) {
+						
+		}
+	}
 }
